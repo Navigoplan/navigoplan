3e56994c1cb94262bf293d6f51847ec3bcb75bed
Navigoplan <navigoplan@gmail.com>
2025-11-09 10:04:10 +0200

    auto: save

--- app/itinerary/final/page.tsx
+++ app/itinerary/final/page.tsx
@@ -1,8 +1,5 @@
 import { Suspense } from "react";
-
-// Αποφεύγουμε prerender/SSG για αυτή τη σελίδα
 export const dynamic = "force-dynamic";
-
 import FinalClient from "./FinalClient";
 
 export default function Page() {
